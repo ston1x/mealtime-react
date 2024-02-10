17e77955e463b0c46fba26825340dac957000634
Nicolai Stoianov <stoianovnk@gmail.com>
2024-02-10 10:37:51 +0100

    Update styling

--- src/App.js
+++ src/App.js
@@ -14,18 +14,24 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <form>
-        <IngredientField
-          ingredients={ingredients}
-          addIngredient={handleAddIngredient}
-          removeIngredient={handleRemoveIngredient}
-        />
+    <div className="App py-4">
+      <section className="h-screen w-screen flex justify-center gap-y-4">
+        <form>
+          <IngredientField
+            ingredients={ingredients}
+            addIngredient={handleAddIngredient}
+            removeIngredient={handleRemoveIngredient}
+          />
 
-        <button onClick={handleSubmit} className="bg-blue-600 text-white outline-none border-none">
-          Search recipes
-        </button>
-      </form>
+          {/* TODO: Make a button component */}
+          <button
+            onClick={handleSubmit}
+            className="button w-full inline-flex justify-center rounded-full border border-transparent bg-indigo-600 py-3 px-4 text-lg font-bold text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Search recipes
+          </button>
+        </form>
+      </section>
     </div>
   );
 }
