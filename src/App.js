ed2eb0851faf458585537a61295aaff22cd3ba15
Nicolai Stoianov <stoianovnk@gmail.com>
2024-02-10 17:25:08 +0100

    Make API call to /recipes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import useIngredientInput from './hooks/useIngredient';
 import IngredientField from './components/IngredientField';
 import './App.css';
+import recipeService from './services/recipeService';
 
 function App() {
   // Retrieve all returned items from the hook
   const { ingredients, handleAddIngredient, handleRemoveIngredient } = useIngredientInput();
 
   // Handle form submission
-  const handleSubmit = () => {
-    // TODO: Send ingredients to the backend
-    console.log(ingredients);
+  const handleSubmit = async () => {
+    try {
+      const response = await recipeService.searchRecipes(ingredients);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
   };
 
   return (
@@ -26,7 +31,6 @@ function App() {
             removeIngredient={handleRemoveIngredient}
           />
 
-          {/* TODO: Make a button component */}
           <button
             onClick={handleSubmit}
             className="button w-full inline-flex justify-center rounded-full border border-transparent bg-indigo-600 py-3 px-4 text-lg font-bold text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
